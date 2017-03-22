97a96fd27d09c9c757be92097677b8b405632d54
Kevin <paledust@hotmail.com>
2017-03-22 00:17:10 -0400

    Multiple Vision Start

---
+++ Missing/Assets/My_Shader/MultiSightShader.shader
@@ -0,0 +1,46 @@
+﻿Shader "Custom/MultiSightShader" {
+	Properties {
+		_Color ("Color", Color) = (1,1,1,1)
+		_MainTex ("Albedo (RGB)", 2D) = "white" {}
+		_Displace("Displace", Vector) = (0.1, 0.1, 0.0, 0.0)
+		_BlurAmount("_BlurAmount", Range(0.0, 1.0)) = 0.0
+	}
+	SubShader
+		{
+			Pass
+			{
+				ZTest Always
+				Cull Off
+				ZWrite Off
+				Fog{Mode Off}
+
+				CGPROGRAM
+				#pragma vertex vert_img
+				#pragma fragment frag
+				#pragma fragmentoption ARB_precision_hint_fastest
+				#include "UnityCG.cginc"
+//				#include "Colorful.cginc"
+
+				sampler2D _MainTex;
+				fixed2 _Displace;
+				float _BlurAmount;
+
+				fixed4 frag(v2f_img i): Color
+				{
+					fixed4 c = tex2D(_MainTex, i.uv);
+
+					c += tex2D(_MainTex, i.uv + half2(_Displace.x * _BlurAmount, _Displace.y * _BlurAmount)) * 1.0f;
+					// c += tex2D(_MainTex, i.uv + half2(_Displace.x * 24, _Displace.y * 24)) * 0.4f;
+					// c += tex2D(_MainTex, i.uv + half2(_Displace.x * 32, _Displace.y * 32)) * 0.2f;
+					
+					c *= 0.5f;
+
+					return c;
+				}
+
+				ENDCG
+			}
+		}
+
+		FallBack off
+}
---
+++ Missing/Assets/Script/DoubleVisionEffect.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;  
+public class DoubleVisionEffect : MonoBehaviour  
+{  
+    public float blurAmount = 0.8f;  
+    public Shader shader;  
+    private Material mat;  
+    public RenderTexture accumTexture;  
+    void Start()  
+    {  
+        mat = new Material(shader); //创建一个材质  
+        mat.hideFlags = HideFlags.HideAndDontSave;  
+    }  
+    void OnRenderImage (RenderTexture src, RenderTexture dst)  
+    {  
+        if (accumTexture == null)  
+        {  
+            accumTexture = new RenderTexture(src.width, src.height, 0);  
+            accumTexture.hideFlags = HideFlags.HideAndDontSave;  
+            Graphics.Blit( src, accumTexture );     //将最初的屏幕画面复制到accumTexture里面  
+        }  
+        blurAmount = Mathf.Clamp( blurAmount, 0.0f, 1f );     
+        mat.SetTexture("_AccumTex", accumTexture);  //指定着色器中参数_AccumTex为accumTexture  
+        mat.SetFloat("_AccumAmt", blurAmount);      //设置着色器中参数_AccumAmt为blurAmount
+
+        Graphics.Blit (src, accumTexture, mat);     //将屏幕当前画面与accumTexture合并  
+        Graphics.Blit(accumTexture, dst);           //将合并后的最终画面输出到dst  
+    }  
+}  
